5a411a1ef79e01a49f70689fa181faaa91aa79f2
Nico Cabezas <51125246+Nicopulus@users.noreply.github.com>
2024-04-03 17:59:22 +0200

    first vscode config, testing

--- Scripts/test_local_global.c
+++ Scripts/test_local_global.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int a = 5;
+
 int main(){
-    printf("It works.");
+    a = 25;
+    {
+        int my_var;
+        my_var = 47;
+        printf("00 - Value of a is: %d\n", a);
+        printf("01 - Value of my_var is: %d\n", my_var);
+    }
+
+    int my_var;
+    my_var = my_var + 10;
+    printf("02_Value of my_var is: %d\n", my_var);
+
     return 0;
 }
